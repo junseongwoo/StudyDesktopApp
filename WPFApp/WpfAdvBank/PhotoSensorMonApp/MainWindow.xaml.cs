2cf5d7c0efd452cc06c275ebffc67a322cae5733
junseongwoo <wnstjd4852@gmail.com>
2021-03-23 16:56:25 +0900

    0323 Wpf 커밋

--- WPFApp/WpfAdvBank/PhotoSensorMonApp/MainWindow.xaml.cs
+++ WPFApp/WpfAdvBank/PhotoSensorMonApp/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using MahApps.Metro.Controls;
-using LiveCharts;
+﻿using LiveCharts;
 using LiveCharts.Wpf;
+using MahApps.Metro.Controls;
+using OxyPlot;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,25 +20,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Threading;
 using System.Windows.Threading;
-using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Data;
-using OxyPlot;
 
 namespace PhotoSensorMonApp
 {
     public class SensorData
     {
         public int Idx { get; set; }
-
         public DateTime CurrentDt { get; set; }
-
         public int Value { get; set; }
-
         public bool SimulFlag { get; set; }
-
         public SensorData(int idx, DateTime currentDt, int value, bool simulFlag)
         {
             Idx = idx;
@@ -42,11 +38,13 @@ namespace PhotoSensorMonApp
             SimulFlag = simulFlag;
         }
     }
+
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
         public MainWindow()
         {
@@ -56,60 +54,62 @@ namespace PhotoSensorMonApp
         }
 
         public ChartValues<int> ChartValues { get; set; }
-        public DispatcherTimer CustomTimer { get; set; }
+
         public int SensorValue { get; set; }
 
-        private  readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public DispatcherTimer CustomTimer { get; set; }
 
-        private string connString = @"Data Source=127.0.0.1;
-                                      Initial Catalog=IoTData;
-                                      User ID=sa;
-                                      Password=mssql_p@ssw0rd!";
+        private string connString = "Data Source=210.119.12.109;" +
+            "Initial Catalog=IoTData;" +
+            "Persist Security Info=True;" +
+            "User ID=sa;" +
+            "Password=mssql_p@ssw0rd!";
 
         public SensorData GetRealTimeSensor()
         {
             SensorData result = null;
 
-            using (SqlConnection conn = new SqlConnection(connString))
+            try
             {
-                try
+                using (SqlConnection conn = new SqlConnection(connString))
                 {
                     if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
 
-                    var query = @"SELECT Idx
-                                       ,CurrentDt
-                                       ,Value
-                                       ,SimulFlag
-                                   FROM Tbl_PhotoResister
-                                  ORDER BY idx DESC ";
+                    var query = @"SELECT TOP 1 Idx
+                                       , CurrentDt
+                                       , Value
+                                       , SimulFlag
+                                    FROM Tbl_PhotoResistor
+                                   ORDER BY Idx DESC ";
 
                     SqlCommand cmd = new SqlCommand(query, conn);
                     var temp = cmd.ExecuteReader();
                     if (temp.Read())
                     {
-                        result = new SensorData(Convert.ToInt32(temp["Idx"]), 
+                        result = new SensorData(Convert.ToInt32(temp["Idx"]),
                                                 Convert.ToDateTime(temp["CurrentDt"]),
                                                 Convert.ToInt32(temp["Value"]),
                                                 Convert.ToBoolean(temp["SimulFlag"]));
                     }
-
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine($"예외발생 : {ex.Message}");
                 }
 
-                return result;
+                logger.Info("GetRealTimeSensor() completed.");
             }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"예외발생 : {ex.Message}");
+                logger.Error($"GetRealTimeSensor() error occured : {ex}");
+            }
+
+            return result;
         }
 
         private void Window_Initialized(object sender, EventArgs e)
         {
             //var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
-            //var y = x.Select(v => Math.Abs(v) < 1e-10 ? 1: Math.Sin(v)/v).ToArray();
+            //var y = x.Select(v => Math.Abs(v) < 1e-10 ? 1 : Math.Sin(v) / v).ToArray();
             //ChtLine.Plot(x, y);
-
-            ChartValues = new ChartValues<int>(); // { 10, 5, 9, 5, 3, 2, 9, 16, 20, 40};
+            ChartValues = new ChartValues<int>(); // { 10, 5, 9, 5, 3, 2, 9, 16, 20, 40 };
             GrdHistory.DataContext = ChartValues;
 
             CustomTimer = new DispatcherTimer();
@@ -120,7 +120,7 @@ namespace PhotoSensorMonApp
 
         private void CustomTimer_Tick(object sender, EventArgs e)
         {
-            SensorValue = GetRealTimeSensor().Value;
+            SensorValue = GetRealTimeSensor().Value;  // new Random().Next(0, 1023);
             GrdRealTime.DataContext = SensorValue;
         }
 
@@ -141,7 +141,7 @@ namespace PhotoSensorMonApp
 
         private void MnuLoad_Click(object sender, RoutedEventArgs e)
         {
-            HistoryValue.ItemsSource = GetHistorySensors();
+            HistoryValues.ItemsSource = GetHistorySensors();
         }
 
         private List<DataPoint> GetHistorySensors()
@@ -152,14 +152,13 @@ namespace PhotoSensorMonApp
             {
                 using (SqlConnection conn = new SqlConnection(connString))
                 {
-                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+                    if (conn.State == ConnectionState.Closed) conn.Open();
 
-                    var query = $@"SELECT idx, Value
-                                    FROM Tbl_PhotoResister
+                    var query = $@"SELECT Idx, Value
+                                    FROM Tbl_PhotoResistor
                                    WHERE CurrentDt > CONVERT(DATETIME, '{DateTime.Now.ToString("yyyy-MM-dd")}')
-                                   ORDER BY Idx;";
+                                   ORDER BY Idx";
 
-                   
                     SqlCommand cmd = new SqlCommand(query, conn);
                     SqlDataReader reader = cmd.ExecuteReader();
 
@@ -168,10 +167,13 @@ namespace PhotoSensorMonApp
                         result.Add(new DataPoint(Convert.ToInt32(reader[0]), Convert.ToInt32(reader[1])));
                     }
                 }
+
+                logger.Info("GetHistorySensors() completed.");
             }
             catch (Exception ex)
             {
-                Debug.WriteLine($"예외발생 : {ex.Message}");
+                Debug.WriteLine($"예외 발생 : {ex.Message}");
+                logger.Error($"GetHistorySensors() error occured : {ex}");
             }
 
             return result;
